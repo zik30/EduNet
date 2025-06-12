57ab0db8f60cde99a2ab1e9adcd1ce7353dea587
zik30 <ainazik.ishenbek@gmail.com>
2025-06-12 13:35:50 +0600

    alias, styles

--- src/app/layout/Layout.jsx
+++ src/app/layout/Layout.jsx
@@ -1,10 +1,9 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
-import { Header } from '../../widgets/header'
-import { Footer } from '../../widgets/footer'
+import { Header } from '@widgets/header'
+import { Footer } from '@widgets/footer'
 
 export const Layout = () => {
-  useScrollTop()
 
   return (
     <>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@app/styles/forward.scss" as *;
+        `,
+      },
+    },
+  },
+
 })
